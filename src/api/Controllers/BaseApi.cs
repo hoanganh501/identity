1f47e0b26a8e9cbe629d6c9727fba74b244c568f
Hoang Anh <hoanganh@example.com>
2026-04-14 16:21:36 +0700

    P-1

--- src/api/Controllers/BaseApi.cs
+++ src/api/Controllers/BaseApi.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     [Authorize]
-    public abstract class BaseApi: Controller
+    public abstract class BaseApi : Controller
     {
+        protected Guid GetCurrentUser()
+        {
+            var userId = User?.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                ?? User?.FindFirst("sub")?.Value;
+
+            if (Guid.TryParse(userId, out var guid))
+                return guid;
+
+            throw new UnauthorizedAccessException("Invalid user id in token");
+        }
     }
 }
